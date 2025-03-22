d09b9331f2e83c4185d36ffda202bea503a06370
Mika <mika.doorenbosch@gmail.com>
2025-03-22 16:18:52 +0100

    got all data

--- set_3/eigenmodes.py
+++ set_3/eigenmodes.py
@@ -1,12 +1,13 @@
 import numpy as np 
 from get_indices import get_indices
 from scipy.linalg import eig
-from scipy.sparse.linalg import eigs
+from scipy.sparse.linalg import eigsh
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import cm
+import time
 
-def get_eigenmodes(L, N, shape="square"):
+def get_eigenmodes(L, N, shape="square", sparse=False):
     M = np.zeros((N*N,N*N))
     if shape == "square" or shape == "rect":
         indices = get_indices(N)
@@ -28,7 +29,7 @@ def get_eigenmodes(L, N, shape="square"):
     else:
         dr = L / (N*2)
         dtheta = 2 * np.pi / N
-        for i in range(1,N-1):
+        for i in range(2,N-1):
             for j in range(N):
                 idx = i * N + j
                 next_r = (i+1)*N + j
@@ -43,54 +44,106 @@ def get_eigenmodes(L, N, shape="square"):
                 M[idx, next_t] = 1 / (r_curr * r_curr * dtheta * dtheta)
                 M[idx, prev_t] = 1 / (r_curr * r_curr * dtheta * dtheta)
         for j in range(N):
-            next_r = N + j
-            M[j, j] = 1
-            M[j, next_r] = -1
-        r = np.linspace(0, L, N)
-        p = np.linspace(0, 2 * np.pi, N)
-        R, P = np.meshgrid(r, p)
+            idx = N + j
+            next_r = (2)*N + j
+            prev_r = 0
+            next_t = i * N + (j+1) % N
+            prev_t = i * N + (j-1) % N
+
+            r_curr = i * dr
+            M[idx, idx] = -2 / (dr * dr) - 2 / (r_curr * r_curr * dtheta * dtheta)
+            M[idx, next_r] = 1 / (dr * dr) + 1/(r_curr * dr)
+            M[idx, prev_r] = 1 / (dr * dr) - 1/(r_curr * dr)
+            M[idx, next_t] = 1 / (r_curr * r_curr * dtheta * dtheta)
+            M[idx, prev_t] = 1 / (r_curr * r_curr * dtheta * dtheta)
+            M[0, 0] = -N / (dr * dr)
+            M[0, j + N] = 1 / (dr * dr)
+        r = np.linspace(0, L/2, N)
+        p = np.linspace(0, 2 * np.pi, N+1, endpoint=True)
+        R, P = np.meshgrid(r, p, indexing="ij")
         X, Y = R *np.cos(P), R*np.sin(P)
-    eigval, eigvec = eig(M)
-    eigval = np.sqrt(eigval*-1)
+        print(M[0])
+    if sparse:
+        eigval, eigvec = eigsh(M)
+    else:
+        eigval, eigvec = eig(M)
+    eigval = np.sqrt(-1*eigval)
     return eigval, eigvec, (X, Y)
 
 def plot_eigenmodes(eigval, eigvec, X, Y, N, L):
     n_plots = 4
     fig = plt.figure()
     smallest_evs = np.argsort(np.where(eigval.real != 0, eigval, np.inf))[:n_plots]
-    print(smallest_evs)
     for q, i in enumerate(smallest_evs):
         o = 221 + q
-        print(o)
         ax = fig.add_subplot(o, projection="3d")
-        ax.plot_surface(X, Y, eigvec[:, i].reshape(N,N), label=f"{eigval[i]}", cmap=cm.coolwarm, linewidth=0, antialiased = False)
-        # print(eigvec[:, i].reshape(N,N))
+        if (N,N) != X.shape:
+            extended = np.zeros((N, N +1))
+            extended[:,:-1] = eigvec[:,i].reshape(N,N)
+            extended[:,-1] = eigvec[:,i].reshape(N,N)[:,0]
+            origin = np.ones_like(extended[0]) * extended[0,0]
+            extended[0] = origin
+            print(eigvec[:,i].reshape(N,N))
+            ax.plot_surface(X, Y, extended, label=f"{eigval[i]}", cmap=cm.coolwarm, linewidth=0, antialiased = False)
+        else:
+            ax.plot_surface(X, Y, eigvec[:, i].reshape(N,N), label=f"{eigval[i]}", cmap=cm.coolwarm, linewidth=0, antialiased = False)
         plt.legend()
     p = eigval.real
-    p_ = np.where(p != 0, p, np.inf)
-    # print(np.argsort(p_))
-    # print(eigval[np.argsort(p_)[:5]])
     plt.show()
 
-def animate_eigenmodes(eigval, eigvec, X, Y, N, L, rank=0):
+def animate_eigenmodes(eigval, eigvec, X, Y, N, L, rank=0, name="membrane"):
     fig = plt.figure()
     smallest_evs = np.argsort(np.where(eigval.real != 0, eigval, np.inf))
     i = smallest_evs[rank]
     ax = fig.add_subplot(111, projection='3d')
     artists = []
-    timeline = np.linspace(0, 100)
+    timeline = np.linspace(0, 5, 500)
+    if (N,N) != X.shape:
+        reshaped = np.zeros((N, N +1))
+        reshaped[:,:-1] = eigvec[:,i].reshape(N,N)
+        reshaped[:,-1] = eigvec[:,i].reshape(N,N)[:,0]
+        origin = np.ones_like(reshaped[0]) * reshaped[0,0]
+        reshaped[0] = origin
+    else:
+        reshaped = eigvec[:,i].reshape(N,N)
+        print(reshaped[-5])
     for t in timeline:
-        container = [ax.plot_surface(X, Y, eigvec[:,i].reshape(N,N) * np.cos(eigval[i] * t), cmap = cm.coolwarm, linewidth = 0 , antialiased=False)]
+        container = [ax.plot_surface(X, Y, reshaped * np.cos(eigval[i] * t), cmap = cm.coolwarm, linewidth = 0 , antialiased=False)]
         artists.append(container)
-    ani = animation.ArtistAnimation(fig = fig, artists=artists)
-    ani.save("membrane.gif")
+    ani = animation.ArtistAnimation(fig = fig, artists=artists, interval=10)
+    ani.save(f"{name}.gif")
     plt.show()
 
     pass
 
 if __name__ == "__main__":
-    L = 100
-    N = 30
+    L = 1
+    N = 33
+    # Plot square eigenmodes
+    eigval, eigvec, (X, Y) = get_eigenmodes(L, N, shape="square")
+    plot_eigenmodes(eigval, eigvec, X, Y, N, L)
+    # animate_eigenmodes(eigval, eigvec, X, Y, N, L, 0)
+    # Plot rectangular eigenmodes
     eigval, eigvec, (X, Y) = get_eigenmodes(L, N, shape="rect")
-    animate_eigenmodes(eigval, eigvec, X, Y, N, L, 1)
-    # plot_eigenmodes(eigval, eigvec, X, Y, N, L)
+    plot_eigenmodes(eigval, eigvec, X, Y, N, L)
+    # Plot circle eigenmodes
+    eigval, eigvec, (X, Y) = get_eigenmodes(L, N, shape="circle")
+    plot_eigenmodes(eigval, eigvec, X, Y, N, L)
+    L_values = [1, 10, 100]
+    plt.cla()
+    for L in L_values:
+        time_s_start = time.time()
+        eigval, _, __ = get_eigenmodes(L, N, shape='square', sparse=True)
+        time_s_stop = time.time()
+        print(f"sparse method with size L = {L}, time: {time_s_stop - time_s_start}")
+        time_n_start = time.time()
+        eigval, _, __ = get_eigenmodes(L, N, shape='square', sparse=False)
+        time_n_stop = time.time()
+        print(f"non-sparse method with size L = {L}, time: {time_n_stop - time_n_start}")
+        plt.yscale("log")
+        plt.plot(np.sort(eigval)[::-1], label=f"L = {L}")
+    plt.legend()
+    plt.show()
+
+
+    # animate_eigenmodes(eigval, eigvec, X, Y, N, L, 1)
